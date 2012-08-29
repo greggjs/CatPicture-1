04cebf6c64fbde6349d3d73da1072a55f9f43a05
Bridget Brenner <brennebr@muohio.edu>
2012-08-29 18:47:52 -0400

    Added a picture

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -1,5 +1,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -7,6 +9,7 @@ using namespace std;
 
 class CatPictureApp : public AppBasic {
   public:
+	void prepareSettings( Settings *settings);
 	void setup();
 	void mouseDown( MouseEvent event );	
 	void update();
@@ -15,10 +18,23 @@ class CatPictureApp : public AppBasic {
 private:
 	float brightness;
 	float brightness2;
+	Surface* mySurface;
+	gl::Texture myImage;
 };
+
+void CatPictureApp::prepareSettings( Settings *settings) 
+{
+	//set the window size to 800 by 600
+	settings->setWindowSize( 800, 600 );
+	settings->setFrameRate( 60.0f );
+}
 
 void CatPictureApp::setup()
 {
+	//Loaded my picture from the Asset folder
+	myImage = gl::Texture( loadImage( loadAsset( "Ballet.jpg" ) ) );
+	//mySurface = new Surface(200,200,false);
+	//Surface ballet(loadImage( loadResources("Ballet.jpeg"));
 	brightness = 0.0;
 	brightness2 = 0.3;
 	
@@ -43,7 +59,10 @@ void CatPictureApp::update()
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( brightness, brightness2, brightness ) ); 
+	
+	gl::clear( Color( brightness, brightness2, brightness ) );
+	gl::draw( myImage, getWindowBounds() );
+	//gl::draw( mySurface* );
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
